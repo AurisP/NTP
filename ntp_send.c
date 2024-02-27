9e83365c751d3dcef8ed64a1c43d6112db623d4a
AurisP <aurispraam@gmail.com>
2024-02-27 05:26:19 +0200

    working ntp_send

--- ntp_send.c
+++ ntp_send.c
@@ -2,7 +2,7 @@
  * POSIX Real Time Example
  * using a single pthread as RT thread
  */
- 
+#include <errno.h> 
 #include <limits.h>
 #include <pthread.h>
 #include <sched.h>
@@ -13,7 +13,7 @@
 #include <time.h>
 #include <lgpio.h>
 #include <unistd.h>
-#define GPIO_PIN 17
+#define GPIO_PIN 18
 /*
 struct period_info {
         struct timespec next_period;
@@ -44,18 +44,26 @@ static void wait_rest_of_period()
         //inc_period(pinfo);
  	struct timespec sleep_time;
 	clock_gettime(CLOCK_REALTIME, &sleep_time);
-	sleep_time.tv_nsec =  1000000000L - sleep_time.tv_nsec;
-
+	//long remaining_nanos = 1000000000L - sleep_time.tv_nsec;
+	sleep_time.tv_nsec = 0;
+	sleep_time.tv_sec += 1;
 	/* for simplicity, ignoring possibilities of signal wakes */
-        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &sleep_time, NULL);
+	//printf("%ld ", sleep_time.tv_sec);
+	int result = clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &sleep_time, NULL);
+    	if (result != 0) {
+        // Handle error (print error message or take appropriate action)
+        fprintf(stderr, "clock_nanosleep failed: %d\n", result);
+    }
+        //clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &sleep_time, NULL);
 }
 
 static void do_rt_task(int h)
 {
         /* Do RT stuff here. */
-    	lgGpioWrite(h,17,1);
-	sleep(0.3);
-	lgGpioWrite(h,17,0);
+	printf("toggle pin");
+    	lgGpioWrite(h,GPIO_PIN,1);
+	sleep(1.3);
+	lgGpioWrite(h,GPIO_PIN,0);
 
 	
 }
@@ -64,6 +72,7 @@ void *thread_func(void *data)
 {
 //      struct period_info pinfo;
 // 	periodic_task_init(&pinfo);
+	printf("started thread/n");
 	int h;
 	h = lgGpiochipOpen(4); // Open the first GPIO chip (gpiochip0)
 	if (h < 0) {
